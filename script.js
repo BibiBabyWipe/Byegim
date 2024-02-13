0cf0b78ee0bcc7b26747d96a94791717ecaef820
BibiBabyWipe <148632892+BibiBabyWipe@users.noreply.github.com>
2024-02-14 03:45:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function drawText() {
 
     if(frameNumber < 250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("Күнделікті мен қаншалықты бақытты екеніме сене алмаймын", canvas.width/2, canvas.height/2);
+        context.fillText("Күнделікті мен қаншалықты", " бақытты екеніме сене алмаймын", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     //fades out the text by decreasing the opacity
@@ -114,7 +114,7 @@ function drawText() {
     }
     if(frameNumber > 1000 && frameNumber < 1250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("тірі болғаным және осы өмірді сенімен бірге өткізетінім", canvas.width/2, canvas.height/2);
+        context.fillText("тірі болғаным және осы өмірді", " сенімен бірге өткізетінім", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     if(frameNumber >= 1250 && frameNumber < 1500){
@@ -185,7 +185,7 @@ function drawText() {
         if (window.innerWidth < 600) {
             drawTextWithLineBreaks(["және мен осы махаббатты сенімен бөлісу үшін ", "дүниедегі барлық уақытымды өткізуді қалаймын"], canvas.width / 2, (canvas.height/2 + 60), fontSize, lineHeight);
         } else {
-            context.fillText("және мен осы махаббатты сенімен бөлісу үшін дүниедегі барлық уақытымды өткізуді қалаймын", canvas.width/2, (canvas.height/2 + 50));
+            context.fillText("және мен осы махаббатты сенімен бөлісу үшін дүниедегі барлық уақытым  ды өткізуді қалаймын", canvas.width/2, (canvas.height/2 + 50));
         }
 
         secondOpacity = secondOpacity + 0.01;
